175387484f86b64d150533d70ff1ab1182c395c6
peterhoejlund <pianopha@gmail.com>
2016-09-23 22:37:58 +0300

    Dine værker, nyt værk virker ikke, men profil er designet

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,19 @@
 class WorksController < ApplicationController
+
+	def index
+	@user = User.find(params[:id])
+	@works = @user.works.all
+	end
+
+	def create
+	user = User.find(params[:work][:user_id])
+    user.answers.create(answer_params)
+	end 
+
+	private
+
+	def work_params 
+		params.require(:work).permit(:headline, :workbody)
+
+	end
 end
