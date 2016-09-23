67471403940fc99f12c5972951ad68826d558599
peterhoejlund <pianopha@gmail.com>
2016-09-23 14:45:50 +0300

    installerer git

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+	
+
+	def new 
+		@user = User.new
+	end
+
+
+	def create
+		user = User.create(user_params)
+		@user_path = user_path(user)
+		session[:current_user_email] = user_params[:email]
+		session[:current_user_username] = user_params[:username]
+		redirect_to @user_path
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		
+	      redirect_to root_path
+ 
+  	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+end
